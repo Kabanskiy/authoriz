a26d17b191faa8591042b635b805e8de8f6990d7
User <batman45@yandex.ru>
2023-03-07 22:01:55 +0300

    добавлен возврат

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+
 class RegisterView(APIView):
     def post(self, request):
          serializer = UserSerializer(data=request.data)
