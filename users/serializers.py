81e12d17e3a74b60a14753ac70ed6ed10397ae7b
User <batman45@yandex.ru>
2023-03-08 18:26:33 +0300

    убрали возвращение пароля

--- users/serializers.py
+++ users/serializers.py
@@ -5,10 +5,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         field = ['id', 'username', 'email', 'password'] # здесь будем создавать поля для нового пользователя
+        extra_kwargs = {"password":{"write_only":True}}
 
         def create(self, validated_data):
-            password=validated_data.pop("password":None)
+            password=validated_data.pop("password", None)
             intance = self.Meta.model(**validated_data)
             if password is not None:
                 intance.set_password(password)
             intance.save()
+            return intance
