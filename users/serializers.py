833656ef9905f731f9b16bc4c2e250caa6915572
User <batman45@yandex.ru>
2023-03-08 18:11:55 +0300

    Создание польз-лся как экземпляр

--- users/serializers.py
+++ users/serializers.py
@@ -5,3 +5,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         field = ['id', 'username', 'email', 'password'] # здесь будем создавать поля для нового пользователя
+
+        def create(self, validated_data):
+            password=validated_data.pop("password":None)
+            intance = self.Meta.model(**validated_data)
+            if password is not None:
+                intance.set_password(password)
+            intance.save()
