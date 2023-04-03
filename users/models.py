da03469df3acff5d0add7bf20a2eebfe2a944f6c
kabanskiy <yarikkabanskiy@mail.com>
2023-04-03 22:21:14 +0300

    добавлены комментарии

--- users/models.py
+++ users/models.py
@@ -4,8 +4,8 @@ from django.forms import CharField
 
 class User(AbstractUser):
     username = models.CharField(max_length=250)
-    email = models.CharField(max_length=250, unique=True)
+    email = models.CharField(max_length=250, unique=True) # уникальное поле, т.к. чз него будет вход
     password = models.CharField(max_length=250)
 
     USERNAME_FIELD = 'email' # поле имя будет заменяться на адрес, т.к. оно уникально
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username'] # список имен полей при создании суперпользователя
