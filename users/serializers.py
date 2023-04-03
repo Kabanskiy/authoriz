4e230b54e70cfe26ce64b492e29274124922c6f1
kabanskiy <yarikkabanskiy@mail.com>
2023-04-03 22:50:40 +0300

    добавлены комментарии

--- users/serializers.py
+++ users/serializers.py
@@ -7,10 +7,10 @@ class UserSerializer(serializers.ModelSerializer):
         field = ['id', 'username', 'email', 'password'] # здесь будем создавать поля для нового пользователя
         extra_kwargs = {"password":{"write_only":True}}
 
-        def create(self, validated_data):
-            password=validated_data.pop("password", None)
-            intance = self.Meta.model(**validated_data)
-            if password is not None:
-                intance.set_password(password)
-            intance.save()
-            return intance
+        def create(self, validated_data): # функция для хеширования пароля
+            password = validated_data.pop("password", None)
+            instance = self.Meta.model(**validated_data)
+            if password is not None: # если пароль не равен нулю, то мы будем его хешировать, т.е. ставить пароль для пароля
+                instance.set_password(password)
+            instance.save()
+            return instance
